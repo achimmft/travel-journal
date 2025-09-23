07d139e8dfdb9d82b389ccf2398ed9a1c5b0bc3a
achimmft <70777695+achimmft@users.noreply.github.com>
2025-09-23 16:56:41 -0600

    refactor(App): render Entry components dynamically with mapped data

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,23 +1,15 @@
 import destinationIcon from "../assets/marker.png";
 
-
 export default function Entry(props) {
-  console.log(props)
   return (
     <article className="journal-entry">
       <div className="main-image-container">
-        <img
-          className="main-image"
-          src={props.img.src}
-          alt={props.alt}
-        />
+        <img className="main-image" src={props.img.src} alt={props.alt} />
       </div>
       <div className="info-container">
         <img className="marker" src={destinationIcon} alt="map marker icon" />
         <span className="country">{props.country}</span>
-        <a href={props.googleMapsLink}>
-          View on Google Maps
-        </a>
+        <a href={props.googleMapsLink}>View on Google Maps</a>
         <h2 className="entry-title">{props.title}</h2>
         <p className="trip-dates">{props.dates}</p>
         <p className="entry-text">{props.text}</p>
